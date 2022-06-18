b450880f5580d7cbb4ce11cded825cb6682be67c
Matthew J. Berger <matthewberger@nevada.unr.edu>
2022-06-18 13:27:31 -0600

    Start adding dependencies

--- crates/phantom_dependencies/src/lib.rs
+++ crates/phantom_dependencies/src/lib.rs
@@ -1,8 +1,4 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub use anyhow;
+pub use env_logger;
+pub use log;
+pub use thiserror;
