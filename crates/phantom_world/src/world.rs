e82e92dcc34571150ed71bc7e9a60de1f04eee8b
Matthew J. Berger <matthewberger@nevada.unr.edu>
2022-09-25 01:39:24 -0600

    feat(world): add default light back for now

--- crates/phantom_world/src/world.rs
+++ crates/phantom_world/src/world.rs
@@ -117,6 +117,7 @@ impl World {
         self.scene = Scene::default();
         self.scene.name = "Main Scene".to_string();
         self.add_default_camera()?;
+        self.add_default_light()?;
         Ok(())
     }
 
