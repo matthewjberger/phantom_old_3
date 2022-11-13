d655fc705be9e55fa4cb4a03640b0690d1589606
Matthew J. Berger <matthewberger@nevada.unr.edu>
2022-11-12 20:24:09 -0700

    refactor(config): explicitly declare dependencies

--- crates/phantom_config/src/config.rs
+++ crates/phantom_config/src/config.rs
@@ -1,33 +1,28 @@
-use phantom_dependencies::serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Serialize, Deserialize)]
-#[serde(crate = "phantom_dependencies::serde")]
 pub struct Config {
     pub graphics: Graphics,
 }
 
 #[derive(Default, Debug, Serialize, Deserialize)]
-#[serde(crate = "phantom_dependencies::serde")]
 pub struct Graphics {
     pub post_processing: PostProcessing,
     pub debug_grid_active: bool,
 }
 
 #[derive(Default, Debug, Serialize, Deserialize)]
-#[serde(crate = "phantom_dependencies::serde")]
 pub struct PostProcessing {
     pub film_grain: FilmGrain,
     pub chromatic_aberration: ChromaticAberration,
 }
 
 #[derive(Default, Debug, Serialize, Deserialize)]
-#[serde(crate = "phantom_dependencies::serde")]
 pub struct ChromaticAberration {
     pub strength: f32,
 }
 
 #[derive(Default, Debug, Serialize, Deserialize)]
-#[serde(crate = "phantom_dependencies::serde")]
 pub struct FilmGrain {
     pub strength: f32,
 }
